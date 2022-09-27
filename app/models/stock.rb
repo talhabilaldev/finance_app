43daaf119fa05a85cccbcc4723b4a6d3bec5621a
Talha Bilal <talhabilal1997@gmail.com>
2022-09-27 20:30:12 +0500

    Add stock model and IEX  gem with keys

---
+++ app/models/stock.rb
@@ -0,0 +1,8 @@
+class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:pub_key],
+            secret_token: Rails.application.credentials.iex_client[:secret_key],
+            endpoint: 'https://cloud.iexapis.com/v1' )
+        client.quote(ticker_symbol).latest_price
+    end
+end
